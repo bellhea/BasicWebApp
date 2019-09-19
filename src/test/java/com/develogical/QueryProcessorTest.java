78990e02b640ca7305b2023957292e68d9814535
Heather Bell <hbell1@sheffield.ac.uk>
2019-09-19 14:35:53 +0100

    request response

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -24,4 +24,9 @@ public class QueryProcessorTest {
     public void isNotCaseSensitive() throws Exception {
         assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
     }
+
+    @Test
+    public void myNameIsShieldedCliffs() throws Exception {
+        assertThat(queryProcessor.process("name?"), containsString("shielded-cliffs"));
+    }
 }
